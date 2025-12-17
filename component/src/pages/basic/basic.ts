6c4ba491779f250ab1cbcd5945dd8c4b7e1ffac9
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-17 23:17:43 +0530

    Session 02

---
+++ component/src/pages/basic/basic.ts
@@ -0,0 +1,49 @@
+import { DatePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+
+
+//   template:`
+//   <h3>This is Template Component</h3>
+
+// <p>Name : {{name}}</p>
+// <p>Marks : {{marks}} </p>
+
+// <p (click)="showMessege()">Click me</p>
+//   `,
+  // styles:[
+  //   `
+  //   h3{
+  //     color :red;
+  //   }
+  //   `
+  // ]
+    selector: 'app-basic',
+  imports: [],
+    templateUrl: './basic.html',
+  styleUrl: './basic.scss',
+standalone:true,
+providers:[DatePipe]
+
+
+
+})
+export class Basic {
+
+  constructor(private date:DatePipe){}
+
+  name:string = "Omkar Mohite hello ";
+  marks:number = 75;
+  liveDate:any;
+
+  showMessege(){
+   alert("In Basic Component Button Clicked");
+  }
+
+  show(){
+    this.liveDate = this.date.transform(new Date(),'dd-MM-yyy');
+  }
+
+}
